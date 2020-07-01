81f41851bfa8d7a55c3466281c27d18e46bb0ebd
maxiao <1156381157@qq.com>
2020-07-01 13:38:11 +0800

    fix config file path

--- message/__init__.py
+++ message/__init__.py
@@ -4,6 +4,6 @@ import os
 
 def getMsg():
 	realPath = os.path.split(os.path.realpath(__file__))[0]
-	count = len(open(realPath+'config/message.txt','r').readlines())#获取行数
+	count = len(open(realPath+'/config/message.txt','r').readlines())#获取行数
 	hellonum=random.randrange(1,count, 1)#生成随机行数
-	return linecache.getline('config/message.txt',hellonum)#随机读取某行
+	return linecache.getline(realPath+'/config/message.txt',hellonum)#随机读取某行
