58a924b6416e6a3e8ec39319a91fa00537b9bc92
maxiao <1156381157@qq.com>
2020-07-02 12:12:10 +0800

    fix send message config

--- message/__init__.py
+++ message/__init__.py
@@ -6,4 +6,4 @@ def getMsg(fileName):
 	realPath = os.path.split(os.path.realpath(__file__))[0]
 	count = len(open(realPath+'/'+fileName,'r').readlines())#获取行数
 	hellonum=random.randrange(1,count, 1)#生成随机行数
-	return linecache.getline(realPath+'/message.txt',hellonum)#随机读取某行
+	return linecache.getline(realPath+'/'+fileName,hellonum)#随机读取某行
